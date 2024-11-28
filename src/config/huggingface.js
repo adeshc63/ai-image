2c4070de7a13f7e08bc07ca6c3717b15ea640e10
adeshc63 <73774380+adeshc63@users.noreply.github.com>
2024-11-28 20:16:07 +0530

    Add files via upload

---
+++ src/config/huggingface.js
@@ -0,0 +1,6 @@
+export const HF_CONFIG = {
+  READ_TOKEN: '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',
+  WRITE_TOKEN: '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',
+  MODEL_ID: 'black-forest-labs/FLUX.1-schnell',
+  API_URL: 'https://api-inference.huggingface.co/models/black-forest-labs/FLUX.1-schnell'
+};
